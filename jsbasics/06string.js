40980da7a50dc0cb0f1729daf7341b983788253e
pankajtech-code <141208842+pankajtech-code@users.noreply.github.com>
2023-08-10 10:05:32 +0000

    ne

---
+++ jsbasics/06string.js
@@ -0,0 +1,11 @@
+const name = "pankaj"
+const repoCount = 10
+console.log(`Hello java script my name is ${name} and my repo count is ${repoCount}`); // good method to declare string
+
+
+// console.log(name + repoCount + " Value"); this is not good method
+
+const gameName = new String('igi')
+
+console.log(gameName[1]);  // define game name character position
+// console.log(gameName);
