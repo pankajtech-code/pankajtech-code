2e23c474ed70b773a8ae884f236dd129c3a6db39
pankajtech-code <141208842+pankajtech-code@users.noreply.github.com>
2023-09-08 11:54:59 +0000

    hhh

---
+++ 01_basics/08datetime.js
@@ -0,0 +1,38 @@
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());  // only date 
+// console.log(myDate.toLocaleString()); // date with time sec
+// console.log(myDate.toLocaleTimeString()); // time am/pm
+// console.log(myDate.toUTCString()); // Tue, 22 Aug 2023 07:45:43 GMT
+// console.log(myDate.getTimezoneOffset()); 
+//console.log(typeof myDate);
+// let myCreatedDate = new Date(2023, 0, 20)     // modified date
+//let myCreatedDate = new Date(2023, 0, 20, 8, 6) 
+ let myCreatedDate = new Date("01-14-2023") 
+ //console.log(myCreatedDate.toLocaleTimeString());
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+ 
+// convert  in sec
+//console.log(Math.floor(Date.now()/1000));
+
+ let newDate = new Date()
+ console.log(newDate);
+ console.log(newDate.getMonth());
+ console.log(newDate.getDay());
+ console.log(newDate.getFullYear());
+ console.log(newDate.getUTCMinutes());
+
+ 
+
+
+
+
